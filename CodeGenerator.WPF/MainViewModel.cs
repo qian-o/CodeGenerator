2ad4677bb3a274e8f646424de4f85fb7156d65a1
熙 王 <1324771795@qq.com>
2022-12-13 10:28:21 +0800

    wx

--- CodeGenerator.WPF/MainViewModel.cs
+++ CodeGenerator.WPF/MainViewModel.cs
@@ -29,7 +29,7 @@ namespace CodeGenerator.WPF
         private void ChangedData()
         {
             Username = "王熙";
-            Code = "123456789011";
+            Code = "123456789011123";
         }
     }
 }
